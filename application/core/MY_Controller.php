ad6eb269d8ee47373d3399fb7bfee2df31d335c2
unknown <mumtazhaqiqy@gmail.com>
2019-02-05 13:03:44 +0700

    Fix Mobile detect

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -13,11 +13,12 @@ class MY_Controller extends MX_Controller
     public function __construct()
     {
         parent::__construct();
-        
+
         $this->config->load('myigniter');
         $this->load->library('layout');
         $this->load->library('ion_auth');
         $this->load->library('message');
+        $this->load->library('mobile_detect');
         $this->load->helper('utility');
 
         $this->layout->sess_language();
--- application/modules/admin/controllers/Admin.php
+++ application/modules/admin/controllers/Admin.php
@@ -12,9 +12,7 @@ class Admin extends MY_Controller
         parent::__construct();
 
 		$this->title = "Admin Data";
-		// $this->load->model('crud_model');
-		$this->load->library('mobile_detect');
-
+		// $this->load->model('crud_model');    
     }
 
     public function create_user()
@@ -27,9 +25,9 @@ class Admin extends MY_Controller
                     'last_name' => 'Edmunds',
                     );
         $group = array('1'); // Sets user to admin.
-    
+
         $this->ion_auth->register($username, $password, $email, $additional_data, $group);
-        
+
         $update_data = array('additional' => '['.json_encode($additional_data).']');
 
         $this->db->where('email',$email);
