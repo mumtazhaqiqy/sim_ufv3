7a7319d2c5fffd0afe6ec5842f402a0680c0fa77
unknown <mumtazhaqiqy@gmail.com>
2019-02-05 12:50:51 +0700

    Fix tanggal Lahir ke format bahasa indonesia

--- application/modules/sertifikasi/views/test.php
+++ application/modules/sertifikasi/views/test.php
@@ -10,14 +10,14 @@
     <?php foreach($data_peserta as $key => $p):?>
     <?= short_if($i, 0, '' , '<pagebreak>')?>
 
-    <div style="position: absolute; top: 155mm; left: 160mm; width: 100mm;">                
+    <div style="position: absolute; top: 155mm; left: 160mm; width: 100mm;">
        <img src="<?=base_url('assets/uploads/image/').$p->photo?>" style="object-fit= cover; width:110px; height: 150px;" width="110px" alt="">
     </div>
-    
+
 
 
     <div style="text-align:center"><b>No. <?= $p->no_sertifikat ?>/SQ/UF - IC/<?=$bulan_tahun_sertifikat?></b></div>
-    
+
     <table style="font-size:15px; line-height:24px" width="100%">
         <tr>
             <td>Diberikan kepada :</td>
@@ -29,7 +29,7 @@
         </tr>
         <tr>
             <td>Tempat, Tanggal Lahir</td>
-            <td>: <?= $p->tempat_lahir?>, <?=$p->tanggal_lahir?></td>
+            <td>: <?= $p->tempat_lahir?>, <?=DatetoIndo($p->tanggal_lahir)?></td>
         </tr>
         <tr>
             <td>Alamat</td>
@@ -37,7 +37,7 @@
         </tr>
         <tr>
             <td colspan="2">
-                Sebagai <b>GURU PENGAJAR AL-QURAN METODE UMMI</b> 
+                Sebagai <b>GURU PENGAJAR AL-QURAN METODE UMMI</b>
             </td>
         </tr>
         <tr>
@@ -54,7 +54,7 @@
             <td  width="30%">Surabaya, <u><?= $tanggal_hijri?></u><br>
             &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
             <span><?=$tanggal_proses?></span>
-        
+
         </td>
         </tr>
         <tr>
@@ -63,7 +63,7 @@
         </tr>
         <tr>
             <td  height="70px">
-            
+
             </td>
             <td></td>
         </tr>
@@ -72,13 +72,13 @@
             <td>Drs. H. Masruri, M.Pd</td>
         </tr>
     </table>
-    <div style="position: absolute; top: 167mm; left: 35mm; width: 100mm;z-index:-1;">                
+    <div style="position: absolute; top: 167mm; left: 35mm; width: 100mm;z-index:-1;">
        <img src="<?=base_url('assets/img/ttd_roem.png')?>" style="" width="200px" style="z-index:-1;" alt="">
     </div>
-    
+
     <?php $i++?>
     <?php endforeach?>
-    
+
 
 
 
